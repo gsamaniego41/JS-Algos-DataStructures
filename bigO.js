5cf21f549fd363eef9d4fc863c44b7de4c04061a
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-02-04 23:14:30 -0800

    Notes: Timing our code

--- bigO.js
+++ bigO.js
@@ -19,3 +19,22 @@ let time1 = performance.now();
 console.log(AddUpTo(1000000000));
 let time2 = performance.now();
 console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds`);
+
+/* 
+Better code === faster?
+Rather than counting seconds which are so variable,
+Count the number of simple operations the computer has to perform
+
+n * (n + 1) / 2;
+3 simple operations, regardless of the size of n
+addition, multiplication, division
+only 3 things that take time
+
+
+for (let i = 1; i <= n; i++) {
+  total += i;
+}
+n operations = if n is 1,000,000,000 then there's 1 billion operations
+n additions, n assignments, i++, comparisons, assignments
+many things that take time
+*/
